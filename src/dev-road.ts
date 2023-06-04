3794208973f22e2c1b628f21ad47fb318659c6ff
Maffie <mrc6113@rit.edu>
2023-06-03 22:34:49 -0400

    basic dev road implementation

--- src/dev-road.ts
+++ src/dev-road.ts
@@ -1,6 +1,12 @@
 export class DevRoad {
     constructor() {}
 
+    /**
+     * Tests if a specified position can be occupied.
+     * @param mile The mile coordinate of the specified position.
+     * @param lane The lane coordinate of the specified position.
+     * @returns True if the specified position is occupiable.
+     */
     isValidPosition(mile:number, lane:number):boolean {
         return true;
     }
