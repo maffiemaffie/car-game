da87a505cec89699d8c5a910023182b1f5396c8b
Maffie <mrc6113@rit.edu>
2023-06-03 22:47:40 -0400

    convert DevRoad to an interface

--- src/dev-road.ts
+++ src/dev-road.ts
@@ -1,13 +1,9 @@
-export class DevRoad {
-    public constructor() {}
-
+export interface DevRoad {
     /**
      * Tests if a specified position can be occupied.
      * @param mile The mile coordinate of the specified position.
      * @param lane The lane coordinate of the specified position.
      * @returns True if the specified position is occupiable.
      */
-    isValidPosition(mile:number, lane:number):boolean {
-        return true;
-    }
+    isValidPosition(mile:number, lane:number):boolean;
 }
